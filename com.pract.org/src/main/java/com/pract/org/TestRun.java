890998652224edb6d3bbb360194ab4afe00e509f
gowtham0324 <49788722+gowtham0324@users.noreply.github.com>
2021-04-02 14:07:14 +0530

    Add files via upload

---
+++ com.pract.org/src/main/java/com/pract/org/TestRun.java
@@ -0,0 +1,12 @@
+package com.pract.org;
+
+public class TestRun {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b= 20;
+
+		System.out.println("Executed the Jekins Run  : "+a+b);
+	}
+
+}
